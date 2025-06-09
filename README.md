d338aef60e21149fd7e2151f399989658b4ec8ae
Dimitri Calixto Arruda Martins <dimitricalixtoarrudamartins@gmail.com>
2025-06-09 20:45:01 -0300

    [UPDATE] Test

---
+++ README.md
@@ -0,0 +1,18 @@
+# Laço Vital (LV)
+
+## Descrição
+
+Este visa criar um desenvolvimento Web de ONG fictícia, onde o usuário pode se cadastrar, fazer login e ver os locais de coleta. A ideia é proporcionar uma plataforma simples e intuitiva para a interação com a comunidade.
+O projeto é uma demonstração de habilidades em desenvolvimento web, desenvolvendo tanto o Back End como o Front End. 
+Não está vinculado a nenhuma organização real.
+
+## Tecnologias Utilizadas
+
+- HTML 5
+- CSS 3
+- JavaScript
+- Bootstrap
+- PHP
+- MySQL
+
+*Observação: Esse projeto vai ser destinado a minha universidade.*
---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!-- includes/header.php -->
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">🩸 Laço Vermelho</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
+        <li class="nav-item"><a class="nav-link" href="agendamento.php">Agendamento</a></li>
+        <li class="nav-item"><a class="nav-link" href="locais.php">Pontos de Doação</a></li>
+      </ul>
+      <a href="login.php" class="btn btn-outline-danger">Login</a>
+    </div>
+  </div>
+</nav>
+
+<!-- Título principal da página -->
+<div class="container mt-5 pt-4">
+  <h1 id="tituloP" class="text-center mt-4">Laço Vermelho</h1>
+  <hr>
+</div>
